5e1b7fa0ba43689bd97898df7be0811d5c6177e4
namelos <namelos@outlook.com>
2015-09-09 18:34:32 +0800

    finish id function

--- app.js
+++ app.js
@@ -10,22 +10,63 @@ app.use(express.static('.'));
 const server = http.createServer();
 const wss = new WSS({ port:8000 });
 
-wss.broadcast = data => {
-  wss.clients.forEach(client => {
-    client.send(JSON.stringify({
-      id: wss.clients.indexOf(client),
-      content: data,
-      portrait: 'http://localhost:5000/jiongun.jpg',
-      date: Date()
-    }));
-  })
-};
+var clients = [];
 
 wss.on('connection', ws => {
-    ws.on('message', message => {
-      console.log(message);
-      wss.broadcast(message);
-    });
+  console.log('connected');
+  clients.push(ws);
+  ws.on('message', msg => {
+    console.log(clients.indexOf(ws));
+    sendAll(msg, ws);
+  });
+  ws.on('close', () => {
+    console.log(clients.indexOf(ws) + ' disconnected');
+    clients.splice(clients.indexOf(ws), 1);
+  });
+  ws.on('error', () => {
+    console.log(clients.indexOf(ws) + ' errored');
+    clients.splice(clients.indexOf(ws), 1);
+  })
 });
+
+function sendAll(msg, ws) {
+  for(var i = 0; i < clients.length; i ++) {
+    var type;
+    if (i === clients.indexOf(ws)) {
+      type = 'self'
+    } else {
+      type = 'others'
+    }
+
+    clients[i].send(JSON.stringify({
+      type: type,
+      content: msg,
+      portrait: 'http://localhost:5000/jiongun.jpg',
+      date: Date.now()
+    }))
+  }
+}
+
+
+
+//wss.broadcast = data => {
+//  wss.clients.forEach(client => {
+//    var type;
+//
+//    client.send(JSON.stringify({
+//
+//      content: data,
+//      portrait: 'http://localhost:5000/jiongun.jpg',
+//      date: Date()
+//    }));
+//  })
+//};
+//
+//wss.on('connection', ws => {
+//    ws.on('message', message => {
+//      console.log(message);
+//      wss.broadcast(message);
+//    });
+//});
 
 app.listen(5000, () => console.log('listening at 5000...'));
